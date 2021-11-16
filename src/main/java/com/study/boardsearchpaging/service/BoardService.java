2389773d30fecff969d74e92e682176fa3dbd505
sosow0212 <sosow0212@naver.com>
2021-11-16 21:07:31 +0900

    .

--- src/main/java/com/study/boardsearchpaging/service/BoardService.java
+++ src/main/java/com/study/boardsearchpaging/service/BoardService.java
@@ -13,11 +13,18 @@ public class BoardService {
     @Autowired // 이 어노테이션은 스프링빈이 알아서 읽어와서 주입을해준다. -> 디펜던시 인젝션
     private BoardRepository boardRepository;
 
+    // 글 작성 및 저장
     public void write(Board board) {
         boardRepository.save(board);
     }
 
+    // 게시글 리스트 모두 불러오기
     public List<Board> boardList() {
         return boardRepository.findAll();
+    }
+
+    // 특정 게시글 불러오기
+    public Board boardView(Integer id) {
+        return boardRepository.findById(id).get();
     }
 }

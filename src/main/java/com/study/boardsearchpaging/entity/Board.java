65584d1d2f520d3d26c8f80e0da12c7584ec555a
sosow0212 <sosow0212@naver.com>
2022-01-10 19:45:02 +0900

    Board Entity 작성시간 추가

--- src/main/java/com/study/boardsearchpaging/entity/Board.java
+++ src/main/java/com/study/boardsearchpaging/entity/Board.java
@@ -2,10 +2,8 @@ package com.study.boardsearchpaging.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -16,10 +14,18 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String writer;
+    private String writer; // 글 작성자 이름
 
     private String title;
 
     private String content;
 
+    private String nickname; // 글 작성자 닉네임
+
+    private LocalDateTime createDate; // 날짜
+
+    @PrePersist // DB에 INSERT 되기 직전에 실행. 즉 DB에 값을 넣으면 자동으로 실행됨
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
 }

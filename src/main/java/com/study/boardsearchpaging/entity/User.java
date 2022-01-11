821fe069d66e8ac8521c8c931b9a18bdacc29b99
sosow0212 <sosow0212@naver.com>
2022-01-11 17:33:12 +0900

    프론트엔드단 구현

--- src/main/java/com/study/boardsearchpaging/entity/User.java
+++ src/main/java/com/study/boardsearchpaging/entity/User.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Builder
 @AllArgsConstructor
@@ -27,10 +28,11 @@ public class User {
 
     private String role; // 권한
 
-    private LocalDateTime createDate; // 날짜
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private LocalDate createDate; // 날짜
 
     @PrePersist // DB에 INSERT 되기 직전에 실행. 즉 DB에 값을 넣으면 자동으로 실행됨
     public void createDate() {
-        this.createDate = LocalDateTime.now();
+        this.createDate = LocalDate.now();
     }
 }
